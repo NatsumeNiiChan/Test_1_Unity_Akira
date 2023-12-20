ef5abee430dbdbb37678ef6121ca2d60bd93cf38
NatsumeNiiChan <natsumeniichan@gmail.com>
2023-12-20 09:51:46 +0100

    WinterSportGame Code angepasst

--- Test_Refaktorisierung/Assets/Scripts/GoodCode/WinterSportsGame.cs
+++ Test_Refaktorisierung/Assets/Scripts/GoodCode/WinterSportsGame.cs
@@ -1,7 +1,6 @@
-using UnityEngine;
-using System;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
+using UnityEngine;
 
 public class WinterSportsGame : MonoBehaviour
 {
@@ -16,17 +15,95 @@ public class WinterSportsGame : MonoBehaviour
     - Nach Abschluss ALLER Refaktorisierungen laden Sie oli90martin@web.de als Collaborator zu Ihrer Git-Repository ein.
     */
 
-    public int playerStamina = 100; public GameObject ski; public List<string> Achievements = new List<string>(); private bool isSkiing; public float speed = 10f;
-    void Start() { playerStamina = 100; Debug.Log("WinterSports beginnt!"); }
-    void Update() { if (playerStamina > 0) { CheckSkiing(); CheckAchievements(); } else { Debug.Log("Spiel beendet!"); } }
+    public List<string> Achievements = new List<string>();
+
+    [SerializeField] private GameObject ski;
+    [SerializeField] private int playerStamina = 100;
+    [SerializeField] private float speed = 10f;
+    private bool isSkiing;
+
+    void Start()
+    {
+        //Staminawert wird festgelegt + Konsolennachricht
+        playerStamina = 100;
+        Debug.Log("WinterSports beginnt!");
+    }
+
+    void Update()
+    {
+        //Abfrage ob der Spieler noch Stamina hat, dann Ausführung von Methoden, sonst Konsolennachricht
+        if (playerStamina > 0)
+        {
+            CheckSkiing();
+            CheckAchievements();
+        }
+        
+        else
+        {
+            Debug.Log("Spiel beendet!");
+        }
+    }
     void CheckSkiing()
-    { if (isSkiing) { var move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); ski.transform.Translate(move * speed * Time.deltaTime); } else { Debug.Log("Spieler kann nicht Ski fahren!"); } }
+    {
+        //Wenn isSkiing true ist wird eine Bewegungsberechnung gestartet, sonst Konsolennachricht
+        if (isSkiing)
+        {
+            var move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            ski.transform.Translate(move * speed * Time.deltaTime);
+        }
+        
+        else
+        {
+            Debug.Log("Spieler kann nicht Ski fahren!");
+        }
+    }
+
     void CheckAchievements()
     {
-        if (Achievements.Count == 0) { Debug.Log("Keine Erfolge erzielt."); } else { foreach (var achievement in Achievements) { Debug.Log("Erfolg: " + achievement); } }
+        //Abfrage ob es Achievements gibt -> Konsolennachricht mit den Achivements
+        if (Achievements.Count == 0)
+        {
+            Debug.Log("Keine Erfolge erzielt.");
+        }
+        
+        else
+        {
+            foreach (var achievement in Achievements)
+            {
+                Debug.Log("Erfolg: " + achievement);
+            }
+        }
     }
-    public void LoseStamina(int amount) { playerStamina -= amount; if (playerStamina < 0) { playerStamina = 0; } }
-    public void AddAchievement(string achievement) { if (!Achievements.Contains(achievement)) { Achievements.Add(achievement); } }
-    public void StartSkiing() { isSkiing = true; }
-    public void StopSkiing() { isSkiing = false; }
+
+    public void LoseStamina(int amount) //wird in diesem Script nicht verwendet
+    {
+        //Staminawert wird verändert, kann nicht unter 0 fallen
+        playerStamina -= amount;
+
+        if (playerStamina < 0)
+        {
+            playerStamina = 0;
+        }
+    }
+
+    public void AddAchievement(string achievement) //wird in diesem Script nicht verwendet
+    {
+        //fügt Achievements hinzu wenn es keine gibt
+        if (!Achievements.Contains(achievement))
+        {
+            Achievements.Add(achievement);
+        }
+    }
+
+    public void StartSkiing() //wird in diesem Script nicht verwendet
+    {
+        //setzt den Bool auf true
+        isSkiing = true;
+    }
+
+    public void StopSkiing() //wird in diesem Script nicht verwendet
+    {
+        //setzt den Bool auf false
+        isSkiing = false;
+    }
 }
